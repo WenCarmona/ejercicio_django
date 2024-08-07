dea61dd43bf5c7ec660fa338033d686675bdb7db
WenCarmona <wencarmona24@hotmail.com>
2024-08-06 20:47:55 -0500

    hola mundo
    ejercicio terminado

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("¡Hola Mundo! Este es mi primer proyecto de Django")
